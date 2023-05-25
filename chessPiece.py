a8b9342c6430f1f382b6d4a11781d3efdd3be5f7
Robert Szeto <robert_szeto@hotmail.co.uk>
2023-05-25 17:02:54 +0100

    added basic structure for all pieces

--- chessPiece.py
+++ chessPiece.py
@@ -9,7 +9,47 @@ class ChessPiece():
 class Pawn(ChessPiece):
     def __init__(self, coord, colour) -> None:
         super().__init__(coord, colour)
+        self.name = "P"
     
     def move(self):
-        #move forward one or 2 or diagonally
+        pass
+
+class Rook(ChessPiece):
+    def __init__(self, coord, colour) -> None:
+        super().__init__(coord, colour)
+        self.name = "R"
+    
+    def move(self):
+        pass
+
+class Knight(ChessPiece):
+    def __init__(self, coord, colour) -> None:
+        super().__init__(coord, colour)
+        self.name = "N"
+    
+    def move(self):
+        pass
+
+class Bishop(ChessPiece):
+    def __init__(self, coord, colour) -> None:
+        super().__init__(coord, colour)
+        self.name = "B"
+    
+    def move(self):
+        pass
+
+class Queen(ChessPiece):
+    def __init__(self, coord, colour) -> None:
+        super().__init__(coord, colour)
+        self.name = "Q"
+    
+    def move(self):
+        pass
+
+class King(ChessPiece):
+    def __init__(self, coord, colour) -> None:
+        super().__init__(coord, colour)
+        self.name = "K"
+    
+    def move(self):
         pass
