c76fc1b718240c239c79ec88d1d33bc4f84931da
Robert Szeto <robert_szeto@hotmail.co.uk>
2023-05-26 14:55:29 +0100

    added board printing stuff

--- board.py
+++ board.py
@@ -13,18 +13,35 @@ class Board():
                     ["P", "P", "P", "P", "P", "P", "P", "P"],
                     ["R", "N", "B", "Q", "K", "B", "N", "R"]]
         self.piece_dict = {
-            'r': Rook(0, 0),
-            "n": Knight(0, 0),
-            "b": Bishop(0, 0),
-            "q": Queen(0, 0),
-            "k": King(0, 0),
-            "p": Pawn(0, 0)}
+            'r': Rook(0, "black"),
+            "n": Knight(0, "black"),
+            "b": Bishop(0, "black"),
+            "q": Queen(0, "black"),
+            "k": King(0, "black"),
+            "p": Pawn(0, "black"),
+            'R': Rook(0, "white"),
+            "N": Knight(0, "white"),
+            "B": Bishop(0, "white"),
+            "Q": Queen(0, "white"),
+            "K": King(0, "white"),
+            "P": Pawn(0, "white"),
+            " ": " "}
 
     def setup(self):
         for count, row in enumerate(self.state):
             mapped_list = pd.Series(row).map(self.piece_dict)
             self.state[count] = mapped_list
         print(self.state)
+    
+    def print_board(self):
+        for row in self.state:
+            temp_row = []
+            for piece in row:
+                if piece != " ":
+                    temp_row.append(piece.get_name())
+                else:
+                    temp_row.append(piece)
+            print(temp_row)
 
 
 
